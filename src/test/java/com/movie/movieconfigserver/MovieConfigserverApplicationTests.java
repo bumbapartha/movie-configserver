d446387803a865d50545ebaef025bc025bac7542
consultant <consultant@192.168.0.104>
2022-01-14 15:12:24 +0530

    Config Server Setup

---
+++ src/test/java/com/movie/movieconfigserver/MovieConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.movieconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
